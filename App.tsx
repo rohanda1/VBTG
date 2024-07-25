436d79a0c9fdeaa97823adc9da20ded0ab032009
Rohan Daran <rohandaran@gmail.com>
2024-07-24 21:57:12 -0700

    Test with simplified package.json and app.tsx files

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { View, Text, Button, Alert } from 'react-native';
+import { Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Welcome to Expo</Text>
-      <Button title="Press me" onPress={() => Alert.alert('Button pressed')} />
+      <StatusBar style="auto" />
     </View>
   );
 }
