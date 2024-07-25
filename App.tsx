29d844b6e2744e22260717615c2e09254ea19957
Rohan Daran <rohandaran@gmail.com>
2024-07-25 14:41:34 -0700

    Successful IOS Expo Go "Hello World Moment" with babel.config.js changes

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
+  console.log('Expo App Online');
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Welcome to Expo</Text>
